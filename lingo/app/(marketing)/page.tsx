ab56032a8b327028f9d7b359f66e20a8958a713f
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-07-13 20:43:36 -0500

    finish footer

--- lingo/app/(marketing)/page.tsx
+++ lingo/app/(marketing)/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
           <h1 className="text-xl leg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
             Learn, pratice, and master new languages with Lingo
           </h1>
+          <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
@@ -51,6 +52,7 @@ export default function Home() {
               </Button>
             </Show>
           </ClerkLoaded>
+          </div>
         </div>
       </div>
     </>
