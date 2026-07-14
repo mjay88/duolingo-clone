2424430dc6bd6ce1efe88ecc1c4d0166cfbec45c
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-07-14 14:42:22 -0500

    add learn page wrappers

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -1,11 +1,24 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/stick-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
+  return (
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        ></UserProgress>
+      </StickyWrapper>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            Learn Page
-        </div>
-    )
-}
-
-
-export default LearnPage; 
+export default LearnPage;
