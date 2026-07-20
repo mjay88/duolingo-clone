29751f25b0171142379a962dd4e133ca0fe935e1
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-07-20 15:46:44 -0500

    course progress - create queries and normalize data for course progress

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -4,38 +4,52 @@ import { StickyWrapper } from "@/components/stick-wrapper";
 import { Header } from "./header";
 import { Unit } from "./unit";
 import { UserProgress } from "@/components/user-progress";
-import { getUnits, getUserProgress } from "@/db/queries";
-
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/db/queries";
 
 const LearnPage = async () => {
-  const userProgressData = getUserProgress()
+  const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
   const unitsData = getUnits();
 
-  const [userProgress, units] = await Promise.all([
-    userProgressData, unitsData
-  ])
-//if no course selected or active redirect
-//this check prevents cascading optional chaining and typing issues
-  if(!userProgress || !userProgress.activeCourse){
-    redirect("/courses")
+  const [userProgress, units, courseProgress, lessonPercentage] = await Promise.all([
+    userProgressData,
+    unitsData,
+    courseProgressData,
+    lessonPercentageData
+    
+  ]);
+  //if no course selected or active redirect
+  //this check prevents cascading optional chaining and typing issues
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
   }
-  
+
+  if(!courseProgress){
+    redirect("/courses");
+  }
+
   return (
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
         {units.map((unit) => (
-         <div key={unit.id} className="mb-10">
-          <Unit 
-          id={unit.id}
-          order={unit.order}
-          description={unit.description}
-          title={unit.title}
-          lessons={unit.lessons}
-          activeLesson={undefined}
-          activeLessonPercentage={0}
-          ></Unit>
-         </div>
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
+            ></Unit>
+          </div>
         ))}
       </FeedWrapper>
       <StickyWrapper>
--- lingo/db/queries.ts
+++ lingo/db/queries.ts
@@ -2,7 +2,13 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { challengeProgress, courses, units, userProgress } from "./schema";
+import {
+  challengeProgress,
+  courses,
+  lessons,
+  units,
+  userProgress,
+} from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -22,13 +28,13 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
-  const {userId} = await auth();
+  const { userId } = await auth();
   const userProgress = await getUserProgress();
 
   if (!userId || !userProgress?.activeCourseId) {
     return [];
   }
-//TODO: confirm whether order is needed
+  //TODO: confirm whether order is needed
   const data = await db.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
@@ -37,18 +43,15 @@ export const getUnits = cache(async () => {
           challenges: {
             with: {
               challengeProgress: {
-                where: eq(
-                  challengeProgress.userId,
-                  userId
-                )
-              }
+                where: eq(challengeProgress.userId, userId),
+              },
             },
           },
         },
       },
     },
   });
-//calculate if a lesson is completed
+  //calculate if a lesson is completed
   const normalizedData = data.map((unit) => {
     const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
       const allCompletedChallenges = lesson.challenges.every((challenge) => {
@@ -60,7 +63,7 @@ export const getUnits = cache(async () => {
       });
       return { ...lesson, completed: allCompletedChallenges };
     });
-    return {...unit, lessons: lessonsWithCompletedStatus}
+    return { ...unit, lessons: lessonsWithCompletedStatus };
   });
   return normalizedData;
 });
@@ -77,4 +80,123 @@ export const getCourseById = cache(async (courseId: number) => {
     //TODO: Populate units and lessons
   });
   return data;
+});
+
+export const getCourseProgress = cache(async () => {
+  const { userId } = await auth();
+  const userProgress = await getUserProgress();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return null;
+  }
+
+  const unitsInActiveCourse = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(units.order)],
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
+        with: {
+          unit: true,
+          challenges: {
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const firstUncompletedLesson = unitsInActiveCourse
+    .flatMap((unit) => unit.lessons)
+    .find((lesson) => {
+      //TODO: if somethind does not work check the last if clause
+      return lesson.challenges.some((challenge) => {
+        return (
+          !challenge.challengeProgress ||
+          challenge.challengeProgress.length === 0 ||
+          challenge.challengeProgress.some(
+            (progress) => progress.completed === false
+          )
+        );
+      });
+    });
+
+  return {
+    activeLesson: firstUncompletedLesson,
+    activeLessonId: firstUncompletedLesson?.id,
+  };
+});
+
+export const getLesson = cache(async (id?: number) => {
+  const { userId } = await auth();
+ //if getting a typescript error, like we were in the where: eq(challengeProgress.userId, userId) call, (asking for method overload), you may need 
+ //to add a null check 
+  if (!userId) {
+    return null;
+  }
+  const courseProgress = await getCourseProgress();
+
+  const lessonId = id || courseProgress?.activeLessonId;
+
+  if (!lessonId) {
+    return null;
+  }
+
+  const data = await db.query.lessons.findFirst({
+    where: eq(lessons.id, lessonId),
+    with: {
+      challenges: {
+        orderBy: (challenges, { asc }) => [asc(challenges.order)],
+        with: {
+          challengeOptions: true,
+          challengeProgress: {
+            where: eq(challengeProgress.userId, userId),
+          },
+        },
+      },
+    },
+  });
+
+  if (!data || !data.challenges) {
+    return null;
+  }
+
+  const normalizedChallenges = data.challenges.map((challenge) => {
+    //TODO: if somethind does not work check the last if clause
+    const completed =
+      challenge.challengeProgress &&
+      challenge.challengeProgress.length > 0 &&
+      challenge.challengeProgress.every((progress) => progress.completed);
+
+    return { ...challenge, completed };
+  });
+
+  return { ...data, challenges: normalizedChallenges };
+});
+
+export const getLessonPercentage = cache(async () => {
+  const courseProgress = await getCourseProgress();
+
+  if (!courseProgress?.activeLessonId) {
+    return 0;
+  }
+
+  const lesson = await getLesson(courseProgress.activeLessonId);
+
+  if (!lesson) {
+    return 0;
+  }
+
+  const completeChallenges = lesson.challenges.filter(
+    (challenge) => challenge.completed
+  );
+  const percentage = Math.round(
+    (completeChallenges.length / lesson.challenges.length) * 100
+  );
+
+  return percentage;
 });
