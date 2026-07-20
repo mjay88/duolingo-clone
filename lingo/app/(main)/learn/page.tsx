5032f5cc5d630f63d646c26f4c1b46b392473193
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-07-20 14:55:18 -0500

    lesson button - calculate button path, add button icons

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/stick-wrapper";
 import { Header } from "./header";
+import { Unit } from "./unit";
 import { UserProgress } from "@/components/user-progress";
 import { getUnits, getUserProgress } from "@/db/queries";
 
@@ -25,7 +26,15 @@ const LearnPage = async () => {
         <Header title={userProgress.activeCourse.title} />
         {units.map((unit) => (
          <div key={unit.id} className="mb-10">
-          {JSON.stringify(unit)}
+          <Unit 
+          id={unit.id}
+          order={unit.order}
+          description={unit.description}
+          title={unit.title}
+          lessons={unit.lessons}
+          activeLesson={undefined}
+          activeLessonPercentage={0}
+          ></Unit>
          </div>
         ))}
       </FeedWrapper>
