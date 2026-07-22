bb75e679a258fa7aa5d9fe995382fc960f8e0f50
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-07-21 21:36:12 -0500

    footer - selecting and styling options, sync audio

--- lingo/app/lesson/quiz.tsx
+++ lingo/app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialPercentage: number;
@@ -34,9 +35,22 @@ export const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
-  const title = challenge.type === "ASSIST" ? "Select the correct meaning" : challenge.question;
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+
+    setSelectedOption(id);
+  };
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
   return (
     <>
@@ -52,21 +66,27 @@ export const Quiz = ({
             <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
               {title}
             </h1>
-            {challenge.type === "ASSIST" && (
+            <div>
+              {challenge.type === "ASSIST" && (
                 <QuestionBubble question={challenge.question} />
-            )}
-            <Challenge 
-            options={options}
-            onSelect={() => {}}
-            status="none"
-            selectedOption={undefined}
-            disabled={false}
-            type={challenge.type}
-
-            />
+              )}
+              <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
           </div>
         </div>
       </div>
+      <Footer 
+      disabled={!selectedOption}
+      status={ status}
+      onCheck={() => {}}
+      />
     </>
   );
 };
