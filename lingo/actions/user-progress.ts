031372609f043e67227b61d2193159f7306c1937
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-07-26 22:38:57 -0500

    shop

--- lingo/actions/user-progress.ts
+++ lingo/actions/user-progress.ts
@@ -9,6 +9,9 @@ import db from "@/db/drizzle";
 import { getCourseById, getUserProgress } from "@/db/queries";
 import { challengeProgress, challenges, userProgress } from "@/db/schema";
 
+
+const POINTS_TO_REFILL = 10;
+
 export const upsertUserProgress = async (courseId: number) => {
   const { userId } = await auth();
   const user = await currentUser();
@@ -118,10 +121,14 @@ export const refillHearts = async () => {
   if (currentUserProgress.hearts === 5) {
     throw new Error("Hearts are already full");
   }
-
+ 
+  if(currentUserProgress.points < POINTS_TO_REFILL){
+    throw new Error("Not enough points")
+  }
  
   await db.update(userProgress).set({
     hearts: 5,
+    points: currentUserProgress.points - POINTS_TO_REFILL,
   }).where(eq(userProgress.userId, currentUserProgress.userId));
 
   revalidatePath("/shop");
